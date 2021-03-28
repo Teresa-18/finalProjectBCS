5abdc90201ff046e2fd1b87170a11bade8a36525
teresa luna <tmluna_18@outlook.com>
2021-03-28 11:43:08 -0500

    adding output_data file

---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+consumer_key = "yQm4Vg5AR3BgMfcErnZz26ZQr"
+
+consumer_secret = "Zf78dYU5sTkIJb3tMbEpyMXDGncdlZX6xMp5naKBUUR5CtCwLO"
+
+access_token = "1109143560996163585-rH4LoOtThJo8FyydnIvaeIvrXLmPDu"
+
+access_token_secret = "qTGs0mvhz39xD99vYc6QFnxQrOMtlULQYM7fZWY2lwdWX"
