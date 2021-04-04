053a269f0311230f75fb35e0104264383e4f4ccb
teresa luna <tmluna_18@outlook.com>
2021-04-04 09:49:20 -0500

    updated nlp_sentiment.py and updated words.csv files

--- python/nlp_sentiment.py
+++ python/nlp_sentiment.py
@@ -30,15 +30,16 @@ from nltk.tokenize import TweetTokenizer
 tokened = TweetTokenizer()
 tokens = tokened.tokenize(text)
 
-
 # def remove_noise(tokens, stop_words = ()):
-
+stop_words = stopwords.words('english')+['0','1','2','3','4','5','6','7','8','9','¦','�','€','[0-9]']
 cleaned_tokens = []
 
 for token, tag in pos_tag(tokens):
     token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
                     '(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token)
     token = re.sub("(@[A-Za-z0-9_]+)","", token)
+
+    token = re.sub("0,1,2,3,4,5,6,7,8,9,¦,�,€","",token)
 
     if tag.startswith("NN"):
         pos = 'n'
@@ -55,73 +56,8 @@ for token, tag in pos_tag(tokens):
 
     # print(cleaned_tokens)
 
-# for tokens in cleaned_tokens:
-#     for token in tokens:
-#         yield token
-
-
-# for tokens in cleaned_tokens_list:
-#     yield dict([token, True] for token in tokens)
-
-
-    # positive_tweets = twitter_samples.strings('positive_tweets.json')
-    # negative_tweets = twitter_samples.strings('negative_tweets.json')
-
-    # positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
-    # negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
-
-    stop_words = stopwords.words('english')
-
-    # positive_cleaned_tokens_list = []
-    # negative_cleaned_tokens_list = []
- 
-    # for tokens in positive_tweet_tokens:
-    #     positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
-
-    # for tokens in negative_tweet_tokens:
-    #     negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
-
-    # # all_pos_words = get_all_words(cleaned_tokens_list)
-
-    # freq_dist_pos = FreqDist(tokens)
-    # # print(freq_dist_pos.most_common(10))
-
-    # positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
-    # negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
-    # cleaned_tokens_for_model = get_tweets_for_model(cleaned_tokens_list=tokens)
-
-    # positive_dataset = [(tweet_dict, "Positive")
-    #                     for tweet_dict in positive_tokens_for_model]
-
-    # negative_dataset = [(tweet_dict, "Negative")
-    #                     for tweet_dict in negative_tokens_for_model]
-
-    # undefined_dataset = [(tweet_dict, "undefined")
-    #                     for tweet_dict in cleaned_tokens_for_model]
-
-    # dataset = positive_dataset + negative_dataset
-    # testset = undefined_dataset
-
-    # random.shuffle(dataset)
-
-    # train_data = dataset[:7000]
-    # test_data = testset[:7000]
-
-    # classifier = NaiveBayesClassifier.train(train_data)
-
-    # print("Accuracy is:", classify.accuracy(classifier, test_data))
-    # print(classifier.show_most_informative_features(10))
-    # print(classifier.classify(dict([token, True] for token in tokens)))
-    # print(tweet_tokens[0])
-    # print(pos_tag(tweet_tokens[0]))
-    # print(remove_noise(tokens, stop_words))
-    # print(positive_tweet_tokens[500])
-    # print(positive_cleaned_tokens_list[500])
-
-freq = nltk.FreqDist(cleaned_tokens)
-for key,val in freq.items():
-    print(str(key) + ':' + str(val))
-freq.plot(20, cumulative=False)
+freq_dist_all = FreqDist(cleaned_tokens)
+print(freq_dist_all.most_common(20))
 
 df = pd.DataFrame(cleaned_tokens)
 
