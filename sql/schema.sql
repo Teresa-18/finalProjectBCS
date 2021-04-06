167f87d330ad4dd8f50cfcd3866761786e0e20fc
Bmaldonado44 <70544086+Bmaldonado44@users.noreply.github.com>
2021-04-05 23:28:58 -0500

    update

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,16 +3,6 @@ DROP TABLE twitter_tweets;
 
 --create table in Twitter_DB
 CREATE TABLE twitter_tweets(
-<<<<<<< HEAD
-    tweets VARCHAR NOT NULL,
-    id BIGINT NOT NULL,
-    len INTEGER,
-    date DATE,
-    source VARCHAR NOT NULL,
-    likes INTEGER,
-    retweets INTEGER,
-    PRIMARY KEY (id)
-=======
     monday_tweets VARCHAR NOT NULL
     friday_tweets VARCHAR NOT NULL,
     april_tweets VARCHAR NOT NULL
@@ -21,7 +11,6 @@ CREATE TABLE twitter_tweets(
     -- likes INTEGER,
     -- retweets INTEGER,
     -- PRIMARY KEY (index)
->>>>>>> 9a4b5f988ec9e25c061e30035d0e95f14bda46b9
 );
 
 -- --create table in Twitter_DB
