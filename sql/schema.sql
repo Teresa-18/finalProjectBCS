53d678e626a9308d486d31f5cc11a6a009968e61
teresa luna <tmluna_18@outlook.com>
2021-04-05 22:36:46 -0500

    update to db_create file

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,14 +3,15 @@ DROP TABLE twitter_tweets;
 
 --create table in Twitter_DB
 CREATE TABLE twitter_tweets(
-    monday_tweets VARCHAR NOT NULL
+    index INTEGER,
+    monday_tweets VARCHAR NOT NULL,
     friday_tweets VARCHAR NOT NULL,
-    april_tweets VARCHAR NOT NULL
+    april_tweets VARCHAR NOT NULL,
     -- date DATE,
     -- source VARCHAR NOT NULL,
     -- likes INTEGER,
     -- retweets INTEGER,
-    -- PRIMARY KEY (index)
+    PRIMARY KEY (index)
 );
 
 -- --create table in Twitter_DB
